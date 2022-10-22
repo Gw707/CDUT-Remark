e681e4c04e580d768a138b49370b8f9e3dddfa73
Gw707 <105411586+Gw707@users.noreply.github.com>
2022-10-22 16:48:46 +0800

    Delete src directory

--- src/main/java/com/hmdp/service/IVoucherOrderService.java
+++
@@ -1,9 +0,0 @@
-package com.hmdp.service;
-
-import com.hmdp.dto.Result;
-import com.hmdp.entity.VoucherOrder;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface IVoucherOrderService extends IService<VoucherOrder> {
-
-}
